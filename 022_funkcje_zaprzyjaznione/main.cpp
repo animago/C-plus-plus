424efa238f445252cbeb38a0de348997834599ae
animago <piotr@animago.pl>
2022-02-08 20:18:59 +0100

    Tests.

--- 022_funkcje_zaprzyjaznione/main.cpp
+++ 022_funkcje_zaprzyjaznione/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void sedzia(Punkt &pkt, Prostokat &p) {
-  p.nazwa = "PODMIANA";
+  // p.nazwa = "PODMIANA";
 
   if ((pkt.x >= p.x) && (pkt.x <= p.x + p.szerokosc) && (pkt.y >= p.y) &&
       (pkt.y <= p.y + p.wysokosc))
@@ -18,15 +18,16 @@ void sedzia(Punkt &pkt, Prostokat &p) {
 }
 
 int main() {
-  Punkt pkt1("A", 3, 17);
+
+  Punkt pkt1("A");
+  // Punkt pkt1("A", 3, 17);
   // pkt1.wczytaj();
 
-  Prostokat p1("Prostokat", 0, 0, 6, 4);
+  Prostokat p1("BOKS");
+  // Prostokat p1("Prostokat", 0, 0, 6, 4);
   // p1.wczytaj();
 
   sedzia(pkt1, p1);
-
-  p1.wczytaj();
 
   return 0;
 }
--- 022_funkcje_zaprzyjaznione/przyjaciele.h
+++ 022_funkcje_zaprzyjaznione/przyjaciele.h
@@ -4,27 +4,24 @@ using namespace std;
 
 class Prostokat;
 
-class Punkt
-{
-   string nazwa;
-   float x,y;
+class Punkt {
+  string nazwa;
+  float x, y;
 
 public:
-   Punkt(string="A",float=0, float=0);
-   void wczytaj();
+  Punkt(string = "A", float = 5, float = 5);
+  void wczytaj();
 
-   friend void sedzia (Punkt &pkt, Prostokat &p);
-
+  friend void sedzia(Punkt &pkt, Prostokat &p);
 };
 
-class Prostokat
-{
-   string nazwa;
-   float x,y,szerokosc,wysokosc;
+class Prostokat {
+  string nazwa;
+  float x, y, szerokosc, wysokosc;
 
 public:
-   Prostokat(string="brak",float=0,float=0,float=1,float=1);
-   void wczytaj();
+  Prostokat(string = "BOKS", float = 0, float = 0, float = 10, float = 10);
+  void wczytaj();
 
-   friend void sedzia (Punkt &pkt, Prostokat &p);
+  friend void sedzia(Punkt &pkt, Prostokat &p);
 };
